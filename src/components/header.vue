0df3343a885d23c516652f365c817232e295139b
Cobb-yoo <yhj010597@naver.com>
2021-11-28 01:49:31 +0900

    navigation

---
+++ src/components/header.vue
@@ -0,0 +1,66 @@
+<template>
+  <app>
+    <v-app-bar app color="primary" elevation="0">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Application </v-list-item-title>
+          <v-list-item-subtitle> subtext </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link :to="item.to">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: true,
+    items: [
+      { title: "Home", icon: "mdi-view-dashboard", to: "/" },
+      { title: "헌금관리", icon: "mdi-image", to: "/about" },
+      { title: "보고서", icon: "mdi-help-box" },
+    ],
+    right: null,
+  }),
+};
+</script>
