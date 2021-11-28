be8a570ac2042921d651d991dbccef7a5ac76f81
Cobb-Yoo <yhj010597@naver.com>
2021-11-28 22:46:42 +0900

    -

--- src/components/header.vue
+++ src/components/header.vue
@@ -59,6 +59,7 @@ export default {
       { title: "Home", icon: "mdi-view-dashboard", to: "/" },
       { title: "헌금관리", icon: "mdi-image", to: "/about" },
       { title: "보고서", icon: "mdi-help-box" },
+      { title: "교인관리", icon: "mdi-help-box" },
     ],
     right: null,
   }),
