dfaa72115a97e803f45efbe117f1d224d8a6c2f0
Cobb-Yoo <yhj010597@naver.com>
2021-12-05 23:29:34 +0900

    반복문으로 table 출력

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,21 @@
         <v-btn @click="setMemberList({ data })" />
         <v-card>
           <v-card-title> 테스트 </v-card-title>
-          <div
-            v-for="member in members"
-            :key="member.id"
-            :v-model="member.name"
-          ></div>
         </v-card>
+
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>이름</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="member in members.slice().reverse()" :key="member.id">
+            <td>{{ member.id }}</td>
+            <td>{{ member.name }}</td>
+          </tr>
+        </tbody>
       </v-col>
     </v-row>
   </v-container>
