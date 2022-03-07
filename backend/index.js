a9a9d22db4b3fc85ec8daab592c161b318bd9549
Cobb-Yoo <yhj010597@naver.com>
2022-03-07 22:58:22 +0900

    -

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const pool = require("./database/pool");
-const port = process.env.PORT || 5000;
-
-app.listen(port, async () => {
-  console.log(`Server is up on port ${port}`);
-  //console.log("Database Connected");
-});
