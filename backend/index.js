ce197c8efbbb0f7a18907cd3f43fc52b896a4631
Cobb-Yoo <yhj010597@naver.com>
2022-03-15 21:25:37 +0900

    -

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,7 @@
 const app = require("./app");
-const { sequelize } = require("./models");
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, async () => {
   console.log(`Server is up on port ${port}`);
-  await sequelize.authenticate();
-  console.log("Database Connected");
 });
