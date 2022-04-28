4005f09eda6d8eb043b802738d52cc3eab85bf41
Cobb-Yoo <yhj010597@naver.com>
2022-04-29 00:01:40 +0900

    성도 등록 table → v-simple-table 로 수정

--- frontend/src/views/manage/Saint.vue
+++ frontend/src/views/manage/Saint.vue
@@ -1,70 +1,84 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <v-col cols="8">
-        <v-card>
-          <v-card-title> 성도 등록 </v-card-title>
-          <v-card-subtitle>
-            여기에 있는 성도에 대해서만 헌금입력이 가능합니다
-          </v-card-subtitle>
+      <v-col cols="6" align="center">
+        <v-card align="center" class="mb-5">
+          <v-row class="ma-0">
+            <v-card-title> 성도 등록 </v-card-title>
+          </v-row>
+
+          <v-row class="ma-0">
+            <v-card-subtitle>
+              여기에 있는 성도에 대해서만 헌금입력이 가능합니다
+            </v-card-subtitle>
+          </v-row>
+
+          <v-col>
+            <v-text-field
+              label="이름"
+              v-model="name"
+              @keyup.enter="check()"
+            ></v-text-field>
+
+            <v-text-field
+              id="직책"
+              label="직책"
+              v-model="position"
+              @keyup.enter="check()"
+            ></v-text-field>
+
+            <v-text-field
+              label="구역"
+              v-model="region"
+              @keyup.enter="check()"
+            ></v-text-field>
+
+            <v-text-field
+              label="나이"
+              v-model="age"
+              @keyup.enter="check()"
+            ></v-text-field>
+
+            <v-radio-group v-model="gender" row>
+              <v-radio label="남자" value="0"></v-radio>
+              <v-radio label="여자" value="1"></v-radio>
+            </v-radio-group>
+          </v-col>
+          <v-btn @click="check()" class="mb-3"> 추가하기 </v-btn>
         </v-card>
-        <v-text-field
-          label="이름"
-          v-model="name"
-          @keyup.enter="check()"
-        ></v-text-field>
+      </v-col>
 
-        <v-text-field
-          id="직책"
-          label="직책"
-          v-model="position"
-          @keyup.enter="check()"
-        ></v-text-field>
-
-        <v-text-field
-          label="구역"
-          v-model="region"
-          @keyup.enter="check()"
-        ></v-text-field>
-
-        <v-text-field
-          label="나이"
-          v-model="age"
-          @keyup.enter="check()"
-        ></v-text-field>
-
-        <v-radio-group v-model="gender" row>
-          <v-radio label="남자" value="0"></v-radio>
-          <v-radio label="여자" value="1"></v-radio>
-        </v-radio-group>
-
-        <v-btn @click="check()"> 추가하기 </v-btn>
-
-        <table>
-          <thead>
-            <tr>
-              <td>이름</td>
-              <td>직책</td>
-              <td>구역</td>
-              <td>나이</td>
-              <td>성별</td>
-              <td></td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="saint in saintList" :key="saint.TAB_ID">
-              <td>{{ saint.NAME }}</td>
-              <td>{{ saint.POSITION }}</td>
-              <td>{{ saint.REGION }}</td>
-              <td>{{ saint.AGE }}</td>
-              <td v-if="saint.GENDER == 0">남자</td>
-              <td v-else>여자</td>
-              <td>
-                <v-btn @click="editting(saint.id)"> 수정 </v-btn>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <v-col cols="6" align="center">
+        <v-simple-table fixed-header height="500px">
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th>이름</th>
+                <th>직책</th>
+                <th>구역</th>
+                <th>나이</th>
+                <th>성별</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="saint in saintList.slice().reverse()"
+                :key="saint.TAB_ID"
+              >
+                <td>{{ saint.NAME }}</td>
+                <td>{{ saint.POSITION }}</td>
+                <td>{{ saint.REGION }}</td>
+                <td>{{ saint.AGE }}</td>
+                <td v-if="saint.GENDER == 0">남자</td>
+                <td v-else>여자</td>
+                <td>
+                  <v-btn @click="editting(saint.id)"> 수정 </v-btn>
+                </td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
       </v-col>
 
       <!-- <v-btn @click="initSaintList()"> 가져오기</v-btn> -->
