69df11369ab6058e33f253acb63ddaea8074d9de
Cobb-Yoo <yhj010597@naver.com>
2022-11-27 23:58:21 +0900

    초기세팅 입력 레이아웃만

--- frontend/src/views/sign/setting.vue
+++ frontend/src/views/sign/setting.vue
@@ -1,55 +1,73 @@
 <template>
-  <v-row justify="center" class="mt-8">
-    <v-stepper>
-      <v-stepper-header>
-        <v-stepper-step :complete="e1 > 1" step="1">
-          Name of step 1
-        </v-stepper-step>
+  <v-container>
+    <v-row>
+      <v-col cols="1">
+        <v-row>
+          <v-col class="mt-8">
+            <v-sheet height="70" width="220" rounded="xl" class="sheets">
+              <span class="sheet-span"> 사용자 권한 </span>
+            </v-sheet>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="mt-8">
+            <v-sheet height="70" width="220" rounded="xl" class="sheets">
+              <span class="sheet-span"> 헌금 목록 추가 </span>
+            </v-sheet>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="mt-8">
+            <v-sheet height="70" width="220" rounded="xl" class="sheets">
+              <span class="sheet-span"> 구역 목록 추가 </span>
+            </v-sheet>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="mt-8">
+            <v-sheet height="70" width="220" rounded="xl" class="sheets">
+              <span class="sheet-span"> 직책 목록 추가 </span>
+            </v-sheet>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="mt-8">
+            <v-sheet height="70" width="220" rounded="xl" class="sheets">
+              <span class="sheet-span"> 부서 목록 추가 </span>
+            </v-sheet>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="mt-8">
+            <v-sheet height="70" width="220" rounded="xl" class="sheets">
+              <span class="sheet-span"> 지출 목록 추가 </span>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-col>
 
-        <v-divider></v-divider>
+      <v-spacer></v-spacer>
 
-        <v-stepper-step :complete="e1 > 2" step="2">
-          Name of step 2
-        </v-stepper-step>
-
-        <v-divider></v-divider>
-
-        <v-stepper-step step="3"> Name of step 3 </v-stepper-step>
-      </v-stepper-header>
-
-      <v-stepper-items>
-        <v-stepper-content step="1">
-          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
-
-          <v-btn color="primary" @click="e1 = 2"> Continue </v-btn>
-
-          <v-btn text> Cancel </v-btn>
-        </v-stepper-content>
-
-        <v-stepper-content step="2">
-          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
-
-          <v-btn color="primary" @click="e1 = 3"> Continue </v-btn>
-
-          <v-btn text> Cancel </v-btn>
-        </v-stepper-content>
-
-        <v-stepper-content step="3">
-          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
-
-          <v-btn color="primary" @click="e1 = 1"> Continue </v-btn>
-
-          <v-btn text> Cancel </v-btn>
-        </v-stepper-content>
-      </v-stepper-items>
-    </v-stepper>
-  </v-row>
+      <v-col cols="10" class="ml-10 mt-8">
+        <v-card class="vcard">
+          <v-card-title> 사용자 추가 </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
-.v-stepper {
-  margin-top: 1%;
-  width: 1400px;
-  height: 600px;
+.sheets {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #98a4f2;
+}
+.sheet-span {
+  font: 1.2em sans-serif;
+}
+.vcard {
+  height: 700px;
 }
 </style>
